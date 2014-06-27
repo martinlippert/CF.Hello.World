4d7db19adc9552c6ca5f9658e20c23e9e4558911
Ken Walker <ken_walker@ca.ibm.com>
2014-06-27 17:38:24 +0000

    Task 10236 fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var http = require('http');
 
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Auto Deploy\n');
