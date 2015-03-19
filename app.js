4c5cf43904dfba6e29bef19e3adf0816e7a38c17
Martin Lippert <mlippert@gmail.com>
2015-03-19 09:35:04 -0400

    changed to martin and added launch config

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hi Gosia from app.js\n');
+  res.end('Hi Martin from app.js\n');
   console.log("here's some logging");
 }).listen(port);
